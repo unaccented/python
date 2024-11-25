5c7f2bd19530772d1509020b2b449c3408337ef7
unaccented <122052631+unaccented@users.noreply.github.com>
2024-11-24 20:22:16 -0600

    Update television.py

--- television.py
+++ television.py
@@ -8,10 +8,10 @@ class Television:
         """
         Initializes the Television instance with default settings.
         """
-        self.__status = False  # TV is initially off
-        self.__muted = False   # TV is initially not muted
-        self.__volume = self.MIN_VOLUME  # Volume set to minimum
-        self.__channel = self.MIN_CHANNEL  # Channel set to minimum
+        self.__status = False  
+        self.__muted = False   
+        self.__volume = self.MIN_VOLUME  
+        self.__channel = self.MIN_CHANNEL  
 
     def power(self):
         """
@@ -23,21 +23,21 @@ class Television:
         """
         Mutes or unmutes the TV.
         """
-        if self.__status:  # Only mute/unmute if the TV is on
+        if self.__status:  
             self.__muted = not self.__muted
 
     def channel_up(self):
         """
         Increases the TV channel. Loops to the minimum channel if the maximum channel is exceeded.
         """
-        if self.__status:  # Only change channels if the TV is on
+        if self.__status:  
             self.__channel = self.MIN_CHANNEL if self.__channel == self.MAX_CHANNEL else self.__channel + 1
 
     def channel_down(self):
         """
         Decreases the TV channel. Loops to the maximum channel if the minimum channel is exceeded.
         """
-        if self.__status:  # Only change channels if the TV is on
+        if self.__status:  
             self.__channel = self.MAX_CHANNEL if self.__channel == self.MIN_CHANNEL else self.__channel - 1
 
     def volume_up(self):
@@ -45,7 +45,7 @@ class Television:
         Increases the TV volume. If the TV is muted, it unmutes and adjusts the volume.
         Volume does not exceed the maximum volume.
         """
-        if self.__status:  # Only adjust volume if the TV is on
+        if self.__status:  
             if self.__muted:
                 self.__muted = False
             if self.__volume < self.MAX_VOLUME:
@@ -56,7 +56,7 @@ class Television:
         Decreases the TV volume. If the TV is muted, it unmutes and adjusts the volume.
         Volume does not go below the minimum volume.
         """
-        if self.__status:  # Only adjust volume if the TV is on
+        if self.__status:  
             if self.__muted:
                 self.__muted = False
             if self.__volume > self.MIN_VOLUME:
