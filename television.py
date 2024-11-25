71da242ec6b8c4bb66c35f6dce64a9bbdd8012c7
unaccented <122052631+unaccented@users.noreply.github.com>
2024-11-24 20:20:04 -0600

    Update television.py

--- television.py
+++ television.py
@@ -5,35 +5,35 @@ class Television:
     MAX_CHANNEL = 3
 
     def __init__(self):
-        self.__status = False  # TV is initially off
-        self.__muted = False   # TV is initially not muted
-        self.__volume = self.MIN_VOLUME  # Volume set to minimum
-        self.__channel = self.MIN_CHANNEL  # Channel set to minimum
+        self.__status = False  
+        self.__muted = False   
+        self.__volume = self.MIN_VOLUME  
+        self.__channel = self.MIN_CHANNEL  
 
     def power(self):
         self.__status = not self.__status
 
     def mute(self):
-        if self.__status:  # Only mute/unmute if the TV is on
+        if self.__status:  
             self.__muted = not self.__muted
 
     def channel_up(self):
-        if self.__status:  # Only change channels if the TV is on
+        if self.__status:  
             self.__channel = self.MIN_CHANNEL if self.__channel == self.MAX_CHANNEL else self.__channel + 1
 
     def channel_down(self):
-        if self.__status:  # Only change channels if the TV is on
+        if self.__status:  
             self.__channel = self.MAX_CHANNEL if self.__channel == self.MIN_CHANNEL else self.__channel - 1
 
     def volume_up(self):
-        if self.__status:  # Only adjust volume if the TV is on
+        if self.__status:  
             if self.__muted:
                 self.__muted = False
             if self.__volume < self.MAX_VOLUME:
                 self.__volume += 1
 
     def volume_down(self):
-        if self.__status:  # Only adjust volume if the TV is on
+        if self.__status:  
             if self.__muted:
                 self.__muted = False
             if self.__volume > self.MIN_VOLUME:
